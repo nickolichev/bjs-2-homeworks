d73b7ece3bd2ef5914a02f30a31fd8787ea7e224
nickolichev@gmail.com <nickolichev@gmail.com>
2023-02-07 19:16:48 +0300

    Задача #1, Задача #2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,56 @@
-﻿
+﻿// Задача #1
+
+function parseCount(value) {
+  const parsedValue = Number.parseFloat(value);
+  if (isNaN(parsedValue)) {
+    throw new Error("Невалидное значение");
+  }
+  return parsedValue;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+
+// Задача #2
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+  get perimeter() {
+  const perimeter = this.a + this.b + this.c;
+  return Number(perimeter.toFixed(3));
+}
+
+get area() {
+  let p = (this.a + this.b + this.c) / 2;
+  return Number((Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3));
+}
+
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+    };
+  }
+}
